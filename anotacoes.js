c1a3fc7426c8b2f37e38f3d28e1275b24037b98a
Juliana Salazar <limajuliana115@yahoo.com.br>
2022-01-11 16:07:19 -0300

    Primeiro commit

---
+++ anotacoes.js
@@ -0,0 +1,49 @@
+/**
+ * Refatorar o código para vermos mais algumas features da ES6
+ * 
+ * Destructing
+ */
+
+ let pessoa = {
+  nome: "Fulano",
+  idade: 21
+}
+let { nome, idade } = pessoa;
+
+let lista = ["Fulano", "Ciclano"];
+
+let [pessoa1, pessoa2] = lista;
+
+/** Spreed Operator
+* Modules
+* 
+* JS Assíncrono
+* Callbacks
+* Promisses
+* Async Await
+*   
+*//**
+ * Refatorar o código para vermos mais algumas features da ES6
+ * 
+ * Destructing
+ */
+
+let pessoa = {
+  nome: "Fulano",
+  idade: 21
+}
+let { nome, idade } = pessoa;
+
+let lista = ["Fulano", "Ciclano"];
+
+let [pessoa1, pessoa2] = lista;
+
+/** Spreed Operator
+* Modules
+* 
+* JS Assíncrono
+* Callbacks
+* Promisses
+* Async Await
+*   
+*/
